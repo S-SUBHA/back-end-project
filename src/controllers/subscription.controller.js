33e7770c49736b6f11043d4a585c251f616b57b5
S-SUBHA <cesubhadip@gmail.com>
2024-06-21 23:50:32 +0530

    Add getUserChannelSubscribers and getSubscribedChannels funtionalities to the subscription controller and corresponding routes

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -59,11 +59,139 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+
+  if (!channelId) {
+    throw new ApiError(400, "INSUFFICIENT INPUT: Channel id is required!");
+  }
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "INVALID INPUT: Improper channel id!");
+  }
+
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: { channel: new mongoose.Types.ObjectId(channelId) },
+    },
+    {
+      $lookup: {
+        from: "users",
+        foreignField: "_id",
+        localField: "subscriber",
+        as: "subscriber",
+        pipeline: [
+          {
+            $project: {
+              _id: 0,
+              username: 1,
+              fullName: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        subscriber: {
+          $first: "$subscriber",
+        },
+      },
+    },
+    {
+      $project: {
+        subscriber: 1,
+        createdAt: 1,
+      },
+    },
+  ]);
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        subscribers,
+        subscriberCount: subscribers.length,
+      },
+      "Subscribers fetched successfully!"
+    )
+  );
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+
+  if (!subscriberId) {
+    throw new ApiError(400, "INSUFFICIENT INPUT: Channel id is required!");
+  }
+
+  if (!isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "INVALID INPUT: Improper channel id!");
+  }
+
+  const channels = await Subscription.aggregate([
+    {
+      $match: { subscriber: new mongoose.Types.ObjectId(subscriberId) },
+    },
+    {
+      $lookup: {
+        from: "users",
+        foreignField: "_id",
+        localField: "channel",
+        as: "channel",
+        pipeline: [
+          {
+            $lookup: {
+              from: "subscriptions",
+              foreignField: "channel",
+              localField: "_id",
+              as: "subscribers",
+            },
+          },
+          {
+            $addFields: {
+              subscriberCount: { $size: "$subscribers" },
+            },
+          },
+          {
+            $project: {
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+              subscriberCount: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        channel: {
+          $first: "$channel",
+        },
+      },
+    },
+    {
+      $project: {
+        channel: 1,
+        createdAt: 1,
+      },
+    },
+  ]);
+
+  // find({
+  //   subscriber: new mongoose.Types.ObjectId(subscriberId),
+  // }).select("-channel -_id -updatedAt -__v");
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        channels,
+        channelsCount: channels.length,
+      },
+      "Channels fetched successfully!"
+    )
+  );
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import {
+  getSubscribedChannels,
+  getUserChannelSubscribers,
+  toggleSubscription,
+} from "../controllers/subscription.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("/:channelId").post(toggleSubscription);
+router
+  .route("/c/:channelId")
+  .get(getUserChannelSubscribers)
+  .post(toggleSubscription);
+router.route("/u/:subscriberId").get(getSubscribedChannels);
 
 export default router;
